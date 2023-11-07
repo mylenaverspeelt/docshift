4268efa10aea2843700ae22ac7e2630b3c99c007
mylenaverspeelt <mylena49@hotmail.com>
2023-11-07 10:39:30 -0300

    change encoding / remove index test page

--- src/main/java/com/project/docshift/controller/ProjectController.java
+++ src/main/java/com/project/docshift/controller/ProjectController.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProjectController {
 
-    @GetMapping("/")
-    public String renderPage() {
-        return "index";
-    }
-
-    @GetMapping("/login")
+   @GetMapping("/login")
     public String renderLoginPage() {
         return "login.xhtml";
     }
